a8bc3e10c008ab243c1d6cadc2ed5f8e926d0ab7
Michelot Alexis <amichelot@club-internet.fr>
2019-11-08 00:34:34 +0100

    numbers fixed, empty slots now reveal good

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ function App() {
   const [squaresValues, setSquaresValues] = useState(emptyFilling);
   const [isBoardSet, setIsBoardSet] = useState(false);
 
-  const emptySlotDiscover = slot => {};
-
   const randomNumbersGeneration = firstClickIndex => {
     let tab = [];
     for (let i = 0; i < 10; i++) {
@@ -30,10 +28,28 @@ function App() {
     return tab;
   };
 
+  const setUpNumbers = (aroundArray, rawFillingArray, i, count, index) => {
+    for (let j = 0; j < aroundArray.length; j++) {
+      if (
+        rawFillingArray[aroundArray[j]] &&
+        rawFillingArray[aroundArray[j]].value === "M"
+      ) {
+        count++;
+      }
+    }
+    if (count !== 0 && rawFillingArray[i].value !== "M") {
+      if (i !== index) {
+        rawFillingArray.splice(i, 1, { value: count, clicked: false });
+      } else {
+        rawFillingArray.splice(i, 1, { value: count, clicked: true });
+      }
+    }
+  };
+
   const showSquare = index => {
     if (!isBoardSet) {
       let rawFillingArray = [...squaresValues];
-      rawFillingArray.splice(index, 1, { value: "", clicked: true });
+      // rawFillingArray.splice(index, 1, { value: "", clicked: true });
 
       //set-up des mines
       let setMinesArray = randomNumbersGeneration(index);
@@ -46,31 +62,51 @@ function App() {
 
       //set-up des chiffres
       for (let i = 0; i < rawFillingArray.length; i++) {
-        let count = 0;
-        let aroundArray = [
-          i - 10,
-          i - 9,
-          i - 8,
-          i - 1,
-          i + 1,
-          i + 8,
-          i + 9,
-          i + 10
-        ];
-        for (let j = 0; j < aroundArray.length; j++) {
-          if (
-            rawFillingArray[aroundArray[j]] &&
-            rawFillingArray[aroundArray[j]].value === "M"
-          ) {
-            count++;
-          }
-        }
-        if (count !== 0 && rawFillingArray[i].value !== "M") {
-          if (i !== index) {
-            rawFillingArray.splice(i, 1, { value: count, clicked: false });
-          } else {
-            rawFillingArray.splice(i, 1, { value: count, clicked: true });
-          }
+        if (i === 0) {
+          let count = 0;
+          let aroundArray = [i + 1, i + 9, i + 10];
+          setUpNumbers(aroundArray, rawFillingArray, i, count, index);
+        } else if (i === 8) {
+          let count = 0;
+          let aroundArray = [i - 1, i + 8, i + 9];
+          setUpNumbers(aroundArray, rawFillingArray, i, count, index);
+        } else if (i === 72) {
+          let count = 0;
+          let aroundArray = [i - 9, i - 8, i + 1];
+          setUpNumbers(aroundArray, rawFillingArray, i, count, index);
+        } else if (i === 80) {
+          let count = 0;
+          let aroundArray = [i - 10, i - 9, i - 1];
+          setUpNumbers(aroundArray, rawFillingArray, i, count, index);
+        } else if (i > 0 && i < 8) {
+          let count = 0;
+          let aroundArray = [i - 1, i + 1, i + 8, i + 9, i + 10];
+          setUpNumbers(aroundArray, rawFillingArray, i, count, index);
+        } else if (i % 9 === 0 && i !== 0 && i !== 72) {
+          let count = 0;
+          let aroundArray = [i - 9, i - 8, i + 1, i + 9, i + 10];
+          setUpNumbers(aroundArray, rawFillingArray, i, count, index);
+        } else if (i % 9 === 8 && i !== 8 && i !== 80) {
+          let count = 0;
+          let aroundArray = [i - 10, i - 9, i - 1, i + 8, i + 9];
+          setUpNumbers(aroundArray, rawFillingArray, i, count, index);
+        } else if (i > 72 && i < 80) {
+          let count = 0;
+          let aroundArray = [i - 10, i - 9, i - 8, i - 1, i + 1];
+          setUpNumbers(aroundArray, rawFillingArray, i, count, index);
+        } else {
+          let count = 0;
+          let aroundArray = [
+            i - 10,
+            i - 9,
+            i - 8,
+            i - 1,
+            i + 1,
+            i + 8,
+            i + 9,
+            i + 10
+          ];
+          setUpNumbers(aroundArray, rawFillingArray, i, count, index);
         }
       }
       setSquaresValues(rawFillingArray);
@@ -79,31 +115,47 @@ function App() {
       let rawFillingArray = [...squaresValues];
       if (rawFillingArray[index].clicked === false) {
         rawFillingArray[index].clicked = !rawFillingArray[index].clicked;
-        let aroundArray = [
-          index - 10,
-          index - 9,
-          index - 8,
-          index - 1,
-          index + 1,
-          index + 8,
-          index + 9,
-          index + 10
-        ];
         if (rawFillingArray[index].value === "") {
-          for (let i = 0; i < aroundArray.length; i++) {
-            if (
-              rawFillingArray[aroundArray[i]] &&
-              rawFillingArray[aroundArray[i]].clicked === false
-            ) {
-              rawFillingArray[aroundArray[i]].clicked = !rawFillingArray[
-                aroundArray[i]
-              ].clicked;
-            }
-          }
+          let checkedSquares = [];
+          rawFillingArray = emptySlotDiscover(
+            rawFillingArray,
+            index,
+            checkedSquares
+          );
         }
         setSquaresValues(rawFillingArray);
       }
     }
+  };
+
+  const emptySlotDiscover = (array, index, checkedSquares) => {
+    let aroundArray = [
+      index - 10,
+      index - 9,
+      index - 8,
+      index - 1,
+      index + 1,
+      index + 8,
+      index + 9,
+      index + 10
+    ];
+    for (let i = 0; i < aroundArray.length; i++) {
+      if (
+        array[aroundArray[i]] &&
+        array[aroundArray[i]].clicked === false &&
+        array[aroundArray[i]].value !== "M"
+      ) {
+        array[aroundArray[i]].clicked = !array[aroundArray[i]].clicked;
+        checkedSquares.push(aroundArray[i]);
+      }
+    }
+    for (let j = 0; j < checkedSquares.length; j++) {
+      if (array[checkedSquares[j]].value === "") {
+        let nextCheckedSquares = [];
+        emptySlotDiscover(array, checkedSquares[j], nextCheckedSquares);
+      }
+    }
+    return array;
   };
 
   return (
