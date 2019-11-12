660da000a0a7c8061751d4d48719bc9d1fa28f8d
Michelot Alexis <amichelot@club-internet.fr>
2019-11-12 13:45:27 +0100

    implemented color for numbers

--- src/components/square/index.js
+++ src/components/square/index.js
@@ -5,15 +5,35 @@ function Square(props) {
   const renderingClick = (value, clicked) => {
     return clicked === "revealed" ? value : null;
   };
+  const renderingColor = value => {
+    if (value === 1) {
+      return "blue";
+    } else if (value === 2) {
+      return "green";
+    } else if (value === 3) {
+      return "red";
+    } else if (value === 4) {
+      return "dark-blue";
+    } else if (value === 5) {
+      return "brown";
+    } else if (value === 6) {
+      return "cyan";
+    } else if (value === 7) {
+      return "purple";
+    } else {
+      return null;
+    }
+  };
   return (
     <button
-      className={
+      className={`${renderingColor(props.value)} ${
         props.clicked === "revealed"
-          ? `square-revealed`
+          ? "square-revealed"
           : props.clicked === "locked"
-          ? `square-locked`
-          : `square-hide`
+          ? "square-locked"
+          : "square-hide"
       }
+      `}
       onClick={() => {
         props.onClickSquare(props.index);
       }}
